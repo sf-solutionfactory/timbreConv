54edf44fe55f04b7c3ab229b7f5c1c234198cadb
Rogelio Sánchez Gómez <rogelio.sanchez@sf-solutionfactory.com>
2022-08-25 18:16:10 -0500

    20220825

--- timbreConver/test/Program.cs
+++ timbreConver/test/Program.cs
@@ -30,12 +30,22 @@ namespace test
         public string Password      = "Conver2021$";
         public string CerPassword   = "Qwertyuiop1";
         public string uuid          = "ece55ed2-c5be-4f68-9967-c1895f321daf";
-        //public string Token = @"T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbXB3YVZxTHdOdHAwVXY2NTdJb1hkREtXTzE3dk9pMmdMdkFDR2xFWFVPUXpTUm9mTG1ySXdZbFNja3FRa0RlYURqbzdzdlI2UUx1WGJiKzViUWY2dnZGbFloUDJ6RjhFTGF4M1BySnJ4cHF0YjUvbmRyWWpjTkVLN3ppd3RxL0dJPQ.T2lYQ0t4L0RHVkR4dHZ5Nkk1VHNEakZ3Y0J4Nk9GODZuRyt4cE1wVm5tbFlVcU92YUJTZWlHU3pER1kySnlXRTF4alNUS0ZWcUlVS0NhelhqaXdnWTRncklVSWVvZlFZMWNyUjVxYUFxMWFxcStUL1IzdGpHRTJqdS9Zakw2UGRDN2VkNEVBUGM1UUt3NVhZVC9QUTViWmZ6dGNJUG5JZVJ1d1hPdmFlN2s3cEp3UW5UY2hORXVWeS9mNnZ6YVZQTlg1OTdBbWJUNzJ4NHFJNVJnOFBxTEo3TGQwank2dlVwektHUmJwY2RqNGdYRG5yaTVZUTBaZ05vR1Y0Z0xsNzg5MlM0cWJUK2hRamV2bXUwcFVGM3E4SzZMNFkvVE5LTCtJZFFEVHNob05QVmRzY2dSUGxBUXBoc29JcVp1TW9MV1FkUUtTRVdROVNPTVRMYkg5dmIrM25LM3pRbDBKN2RHaEI5TDZLK2hqVUhJU3RsZ3dEeGc0NnlWUXUvZEpmc3F6c1pNZHF4YitvYzhLQ1BSWW1vejE2ZGNNVHdETitIckl3OGhVRXFSZFFGY2lQSktqQW5LRWdCNm1jT2VzQmR4TWxFRXg1NTFXZ1UzSGNobTNXbGtUaUo5cmNucnYrWXM5cVQ0Q0NlODFPaldKZjVTRHR6alNodjc0VFgwZGE.loaXVczHpJjV8E_3NYByEmxRJKHFCS0qHPOr7LJKtPM";
         //CONVERGRAM
         public string Rfc           = "CME930714K32";
         public string noCertificado = "00001000000505173025";
-        public string Cer           = Convert.ToBase64String(File.ReadAllBytes("../../../Resources/CME930714K32.cer"));
-        public string Key           = Convert.ToBase64String(File.ReadAllBytes("../../../Resources/CME930714K32.key"));
+        public string Cer           = Convert.ToBase64String(File.ReadAllBytes("../../../Resources/CERC010.cer"));
+        public string Key           = Convert.ToBase64String(File.ReadAllBytes("../../../Resources/KEYC010.key"));
+
+
+        //public string User = "gustavom@conver.com.mx";
+        //public string Password = "Conver2021$";
+        //public string CerPassword = "12345678a";
+        //public string uuid = "ece55ed2-c5be-4f68-9967-c1895f321daf";
+        ////CONVERGRAM
+        //public string Rfc = "IIA040805DZ4";
+        //public string noCertificado = "30001000000400002427";
+        //public string Cer = Convert.ToBase64String(File.ReadAllBytes("../../../Resources/CSD_IIA040805DZ4_20190617_133143s.cer"));
+        //public string Key = Convert.ToBase64String(File.ReadAllBytes("../../../Resources/CSD_IIA040805DZ4_20190617_133143.key"));
         //public string Pfx = Convert.ToBase64String(File.ReadAllBytes("Resources/CertificadosDePrueba/CSD_XIA190128J61.pfx"));
         //public byte[] Acuse = File.ReadAllBytes("Resources/acuse.xml");
         //public byte[] RelationsXML = File.ReadAllBytes("Resources/RelationsXML.xml");
